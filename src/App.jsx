54ae331c0597d1b25a481aaf5cdc6c643972fc21
Ikevine <ishimwekevine098@gmail.com>
2023-07-26 11:05:28 +0200

    structure of website

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
-
+import style from './style'
+import {NavBar , Hero , Stats , Business , Billing , CardDeal , Testimonial , Client , CTA , Footer } from './components'
 const App = () => {
   return (
-    <div className='bg-primary w-full'>App</div>
+    <div className="bg-primary w-full overflow-hidden">
+
+      {/* navbar */}
+       <div className={`${style.paddingX} ${style.flexCenter}`}>
+         <div className={`${style.boxWidth}`}>
+           <NavBar/>
+         </div>
+       </div>
+
+       {/* Hero section */}
+       <div className={`bg-primary ${style.flexStart}`}>
+         <div className={`${style.boxWidth}`}>
+            <Hero/>
+         </div>
+       </div>
+
+       {/* Other section start here */}
+       <div className={`${style.paddingX} bg-primary ${style.flexStart}`}>
+         <div className={`${style.boxWidth}`}>
+             <Stats/>
+             <Business/>
+             <Billing/>
+             <CardDeal/>
+             <Testimonial/>
+             <Client/>
+             <CTA/>
+             <Footer/>
+         </div>
+       </div>
+
+     {/* container end here */}
+    </div>
+    
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,22 @@
+import NavBar from "./NavBar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Client from "./Client";
+import CTA from "./CTA";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Stats from "./Stats";
+import Testimonial from "./Testimonial";
+import Business from "./Business";
+export {
+    NavBar,
+    Billing,
+    CardDeal,
+    Client,
+    CTA,
+    Footer,
+    Business,
+    Hero,
+    Stats,
+    Testimonial
+}
