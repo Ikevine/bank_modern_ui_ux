d8747fd9008f754902b76dfd7ed84d0b986d9214
Ikevine <ishimwekevine098@gmail.com>
2023-07-28 03:00:15 +0200

    cardDeal

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import {card} from '../assets'
+import styles , {layout} from '../style'
+import Button from './Button'
 
-const CardDeal = () => {
-  return (
-    <div>CardDeal</div>
-  )
-}
-
+const CardDeal = () => (
+  <section className={layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden'/> in few easy steps</h2>
+        <p className={styles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed pariatur in repellendus fugiat, explicabo nostrum.</p>
+        <Button styles="mt-10"/>
+      </div>
+      <div className={layout.sectionImg}>
+         <img src={card} alt="card_deal" className='w-[100%] h-[100%]' />
+      </div>
+  </section>
+)
 export default CardDeal
--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import {feedback} from '../constants'
+import styles from '../style'
+import FeedbackCard from './FeedbackCard'
 
-const Testimonial = () => {
-  return (
-    <div>Testimonial</div>
-  )
-}
+const Testimonial = () => (
+  
+  <section id='client' className={`${styles.flexCenter} ${styles.paddingY} relative flex-col`}>
+     <div className="w-full flex justify-center items-center z-[1] relative mb-6 md:mb-16 flex-col md:flex-row ">
+      <h2 className={styles.heading2}>What people are <br className='sm:block hidden'/> saying about us</h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} mx-w-[450px] text-left`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, repudiandae! Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
+     </div>
+
+     {/* cards */}
+     <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+       {feedback.map((card)=>(
+           FeedbackCard
+       ))}
+     </div>
+  </section>
+)
 
 export default Testimonial
